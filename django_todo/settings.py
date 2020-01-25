6df8407081e7c6efc4c426d1c0f1b03eb3109228
Michael Brown <brownie_124@yahoo.com>
2020-01-25 11:41:17 +0000

    Changed to look at secret_key environ var

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4ry^*^cpdtm%8-vly#c%8w2r6h^855dwm!*%b_ot6*y)^ids_j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = dev
